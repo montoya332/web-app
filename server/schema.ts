c8f7f142a7f662b8da192830d644465c19b242d4
Arturo Montoya <montoya332@live.com>
2018-07-06 14:26:01 -0700

    json server

--- server/schema.ts
+++ server/schema.ts
@@ -1,51 +1,25 @@
 const {gql} = require('apollo-server');
-const users = require('./stubData/users.js');
 const {find, filter} = require('lodash');
-
+const axios = require('axios');
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
   # Comments in GraphQL are defined with the hash (#) symbol.
 
   # This "User" type can be used in other type declarations.
-  type TypeUserName {
-    title: String
-    first: String
-    last: String
-  }
-  type TypeLocation {
-    street: String
-    city: String
-    state: String
-    postcode: Int,
-  }
-  type TypeDob {
-    date: String
-    age: Int
-  }
-  type TypeUserRegistered {
-    date: String
-    age: Int
-  }
-  type TypeUserPicture {
-    large: String
-    medium: String
-    thumbnail: String
-  }
   type User {
-    gender: String  
-    email: String
-    phone: String
-    cell: String
-    id: String
-    nat: String
-    name: TypeUserName
-    location: TypeLocation
-    dob: TypeDob
-    registered: TypeUserRegistered
-    picture: TypeUserPicture
+    id: String,
+    name: String,
+    username: String,
+    email: String,
+    phone: String,
+    website: String,
   }
-
+  type Post {
+    id: String
+    title: String 
+    authorId: String!
+  }
   # The "Query" type is the root of all GraphQL queries.
   # (A "Mutation" type will be covered later on.)
   type Query {
@@ -54,14 +28,14 @@ const typeDefs = gql`
   }
 `;
 
-const usersList = users(50);
 
 // Resolvers define the technique for fetching the types in the
 // schema.  We'll retrieve users from the "users" array above.
+const baseUrl = 'http://localhost:3000';
 const resolvers = {
   Query: {
-    users: (root, args, context, info) => { console.log(root, args, context, info); return usersList; },
-    user: (root, args) => find(usersList, x => args.id === x.id)
+    users: (root, args, context, info) => axios.get(`${baseUrl}/users`).then(x => x.data),
+    user: (root, args) => axios.get(`${baseUrl}/users/${args.id}`).then(x => x.data)
   }
 };
 
